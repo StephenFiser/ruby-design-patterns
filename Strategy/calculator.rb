c564593dd35ea7b3ca4f6b37d4590f78ceb1fbd1
unknown <fisers@LCANB722.dds.dillards.net>
2013-08-30 09:18:13 -0500

    added comments

--- Strategy/calculator.rb
+++ Strategy/calculator.rb
@@ -1,12 +1,12 @@
 class Calculator # This is just to illustrate the strategy pattern. Don't build a calculator class like this...
 	attr_accessor :operation, :x, :y
 
-	def initialize(operation)
+	def initialize operation=nil # optionally pass in an instance of the operation you want to be performed (the 'Strategy')
 		@operation = operation
 	end
 
 	def perform
-		@operation.perform(self)
+		@operation.perform self
 	end
 
 end
